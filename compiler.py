fc2feaca7fd9a1bd4187b818e6dd6e8cfe3390f1
Kirill <mbed@mail.kirill.ca>
2013-11-03 18:37:59 -0500

    Initial commit
    print not 123 compiles, lists are not even tried.
    TODO: clean up let[] emulation

---
+++ compiler.py
@@ -0,0 +1,203 @@
+
+import numbers, ts
+
+logolocals = []
+
+
+
+def test(x):
+    global result
+    result = []
+    pass3(x)
+    return result
+
+def run_line(line):
+    ts.init(line)
+    print compile_line(ts.readList())
+
+def compile_line(code):
+    global result, pc
+    result = arglist = locals = []
+    pc = 0
+    name = '*toplevel*'
+    
+    global istoplevel
+    istoplevel = False
+    
+    global body
+    body = code
+    pass2_body()
+    return pass3(result[:]) + [0]
+
+def pass2_body():
+    global command
+    command = True
+    while ([] != body):
+        pass2_item(body.pop(0))
+
+def pass2_item(item):
+    if isinstance(item, list): pass2_list(item)
+    elif isinstance(item, int): pass2_number(item)
+    elif isinstance(item, float): pass2_item(floatbits(item))
+    elif isinstance(item, str): pass2_string(item)
+    elif isinstance(item, ts.dsym): pass2_dsym(item)
+    else: pass2_symbol(item)
+
+def pass2_number(n):
+    if -1 < n and n < 256:
+        add_and_count(['byte', n], 2)
+    else:
+        add_and_count(['number', n], 5)
+
+def pass2_string(s):
+    s = s.replace('\\n', '\n')
+    add_and_count(['string', s], len(s))
+
+def pass2_list(item):
+    if item != []:
+        # deal with ## and #
+        return
+    
+    global command
+    command = True
+    add_and_count(['-[-', 0], 3)
+    pass2_body(item)
+    add_and_count(['-]-', 0], 1)
+
+def pass2_dsym(item):
+    offset = dsym_offset(item)
+    add_and_count(['lthing', offset], 2)
+
+def dsym_offset(item):
+    if item in arglist: return len(arglist) - arglist.index[item] - 1
+    return 0xff & 0 - locals.index[item] - 1
+
+def pass2_symbol(item):
+    nargs = item.args
+    if nargs < 0: raise ValueError('not enough inputs to ' + item)
+    if command:
+        if item.outputs: raise ValueError("you don't say what to do with " + item)
+    else:
+        if not item.outputs: raise ValueError(item + " doesn't output")
+    
+    pass2_argloop(nargs)
+    pass2_funcall(item)
+
+def pass2_argloop(nargs):
+    global istoplevel
+    let_istoplevel = istoplevel
+    istoplevel = False
+    
+    global command
+    let_command = command
+    command = False
+        
+    if len(body) == 0: raise ValueError('not enough inputs to ' + item)
+    pass2_item(body.pop(0))
+    
+    istoplevel = let_istoplevel
+    command = let_command
+
+def pass2_funcall(item):
+    # handle ufun
+    if item.type == 'external': add_and_count(['external', item], 2)
+    elif item.special: handle_special(item)
+    else: add_and_count(['prim', item], 1)
+
+
+def pass3(body):
+    global result
+    result = []
+    while ([] != body):
+        pass3_item(body.pop(0))
+
+    return result
+
+def pass3_item(item):
+    x = item[2]
+    selectq(item[1],
+    [
+        ('to', lambda: add(x.args, x.locals)),
+        ('byte', lambda: add(1, byte(0, x))),
+        ('number', lambda: add(2, byte(0, x), byte(1, x), byte(2, x), byte(3, x))),
+        ('string', lambda: pass3_string(x)),
+        ('prim', lambda: add(prim(x))),
+        ('external', lambda: add_ext(x)),
+    ])
+
+def pass3_string(s):
+    add(3, byte(0, 1 + len(s)), byte(1, 1 + len(s)))
+    map(add, [ord(c) for c in s])
+
+def byte(shift, x):
+    shift *= 8
+    return (x & (0xff << shift)) >> shift
+
+def selectq(x, fcns):
+    for each in fcns:
+        token, fcn = each
+        if token == x: return fcn()
+
+def add_and_count(item, len):
+    global pc
+    add([pc] + item)
+    pc += len
+    
+def add(*args):
+    map(lambda x: result.append(x), args)
+
+def setup_prims(primtype, dest, *newprims):
+    for x in newprims:
+        name, outputs, args = x
+        sym = ts.intern(name)
+        sym.args = args
+        sym.outputs = outputs
+        if sym.args < 0: infixes.append(sym)
+        sym.type = primtype
+        sym.special = False
+        dest.append(sym)
+
+def setup_specials(*newprims):
+    for x in newprims:
+        sym = ts.intern(x)
+        sym.type = 'prim'
+        sym.special = True
+
+def prim(x):
+    return prims.index(x) + 12
+
+def add_ext(x):
+    if x.outputs:
+        add(11)
+    else:
+        add(10)
+    
+    add(externals.index(x))
+
+prims = []
+setup_prims('prim', prims,
+    ('not', True, 1)
+)
+
+externals = []
+setup_prims('external', externals,
+    ('print', False, 1)
+)
+
+infixes = []
+
+setup_specials('(', ')', 'waituntil', 'make', 'let')
+
+class record: pass
+
+class dsym: pass
+
+class symbol:
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return '<symbol: ' + self.name + '>'
+
+    def __str__(self):
+        return self.name
---
+++ ts.py
@@ -0,0 +1,125 @@
+source = ''
+offset = 0
+oblist = {}
+
+def init(s):
+    global source, offset
+    source = s
+    offset = 0
+    skipSpace()
+
+def eof():
+    return len(source) == offset
+
+def readList():
+    result = []
+    while not eof():
+        token = readToken()
+        if (None != token): result.append(token)
+    return result
+
+def readToken():
+    s = next();
+    if len(s) == 0: return intern(s)
+    
+    if s[0] == '$':
+        try: return int(s[1:], 16)
+        except ValueError: pass
+    
+    if s[0] == '"': return qsym(intern(s[1:]))
+    if s[0] == ':': return dsym(intern(s[1:]))
+    if s[0] == '|': return s[1:]
+    if s[0] == ']': return None
+    if s[0] == '[': return readList()
+    if s[0] == '#':
+        try: return int(s[1:], 2)
+        except ValueError: pass
+    
+    if s[-1] == 'f':
+        try: return float(s[:-1])
+        except ValueError: pass
+
+    try: return int(s)
+    except ValueError: pass
+    
+    try: return float(s)
+    except ValueError: pass
+    
+    try: return int(s, 8)
+    except ValueError: pass
+    
+    try: return int(s, 16)
+    except ValueError: pass
+    
+    return intern(s)
+
+def next():
+    s = ''
+    if not delim(peekChar()):
+        while not eof() and not delim(peekChar()):
+            if peekChar() == '|':
+                s += getVbarString()
+                skipSpace()
+                return s
+            else:
+                s += nextChar()
+    else:
+        s = nextChar()
+    
+    skipSpace()
+    return s
+
+def getVbarString():
+    s = ''
+    nextChar()
+    while not eof():
+        if peekChar() == '|':
+            nextChar()
+            break
+        else: s += nextChar()
+    
+    return s
+
+def peekChar():
+    return source[offset]
+
+def nextChar():
+    global offset
+    pos = offset
+    offset += 1
+    return source[pos]
+
+def delim(s): return "()[] ,\t\r\n".find(s[0]) != -1
+
+def skipSpace():
+    global offset
+    while not eof() and ' ;,\t\r\n'.find(peekChar()) != -1:
+        if peekChar() == ';':
+            while not eof() and '\r\n'.find(peekChar()) == -1:
+                offset += 1
+        else: offset += 1
+
+def intern(s):
+    if len(s) == 0: lcstr = s
+    elif s[0] == '|': lcstr = s[1:]
+    else: lcstr = s.lower()
+    sym = oblist.get(lcstr)
+    if sym == None:
+        sym = symbol(s)
+        oblist[lcstr] = sym
+    return sym
+
+class symbol:
+    def __init__(self, pname): self.pname = pname
+    
+    def __repr__(self): return '<symbol: ' + self.pname + '>'
+
+class qsym:
+    def __init__(self, sym): self.sym = sym
+    
+    def __repr__(self): return '<qsym: ' + self.sym.pname + '>'
+        
+class dsym:
+    def __init__(self): self.sym = sym
+    
+    def __repr__(self): return '<dsym: ' + self.sym.pname + '>'
