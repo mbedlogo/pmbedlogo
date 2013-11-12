08b1e52ec57956f6414fb23afdb79b53141e7d61
Gary Servin <garyservin@gmail.com>
2013-11-11 22:33:08 -0300

    write_flash needs to send data in blocks of 256 bytes solved, minor corrections

--- uartcomms.py
+++ uartcomms.py
@@ -1,5 +1,4 @@
 
-import os
 import serial
 from serial.tools import list_ports
 
@@ -23,29 +22,25 @@ class mbedLogo():
         return self.mbed.inWaiting()
 
     def read(self):
-        """Reads count number of bytes from the mbed
-
-        Args:
-            mbed: mbed serial device
-
-        """
+        """Reads response from the mbed"""
         response = []
         while True:
             data = self.mbed.read()
-            if data != '' and data != chr(255) and data != '\n':
+            if data != '':
                 response.append(ord(data))
-                #print ord(data),
             else:
                 break
         return response
 
     def read_ascii(self):
+        """Reads response from the mbed in ASCII"""
         response = self.read()
         if len(response) != 0:
             response_str = ''
             for i in response:
-                response_str = response_str + chr(i)
-            return '  %s\r\n' %response_str
+                if i != 255 and i != '\n':
+                    response_str = response_str + chr(i)
+            return '  %s\r' %response_str
         else:
             return '\r'
 
@@ -93,7 +88,7 @@ class mbedLogo():
 
         """
         self.write(0xff)
-        return self.read()[0]
+        return self.read()
 
     def read_memory(self, address, count):
         """Reads count bytes from memory starting at address
@@ -111,13 +106,12 @@ class mbedLogo():
         self.write16(count)    # Send how many bytes we want to read (2 bytes)
         return self.read()    # Read the number of bytes from the device
 
-    def write_memory(self, address, count, data):
-        """Writes count bytes to the RAM memory starting at address
+    def write_memory(self, address, data):
+        """Writes bytes to the RAM memory starting at address
 
         Args:
             mbed:     mbed serial device
             address:  start address to write
-            count:    number of bytes
             data:     list of bytes
         Returns:
             a number 0
@@ -125,31 +119,36 @@ class mbedLogo():
         """
         self.write(0xfd)        # Send the write memory opcode
         self.write32(address)    # Send the 32 bits destination address (4 bytes)
-        self.write16(count)    # Send how many bytes we want to write (2 bytes)
+        self.write16(len(data))    # Send how many bytes we want to write (2 bytes)
         for i in range(len(data)):    # Write data
             self.write(data[i])
-        return 0
+        return self.read()
 
-    def write_flash(self, sector, address, count, data):
-        """Writes count bytes to the FLASH memory starting at address in sector
+    def write_flash(self, sector, address, data):
+        """Writes bytes to the FLASH memory starting at address in sector
 
         Args:
             mbed:     mbed serial device
             sector:   sector of the FLASH memory
             address:  start address to write
-            count:    number of bytes
             data:     list of bytes
         Returns:
             zero if the write was sucesfull
 
         """
-        self.write(0xfb)        # Send the write flash opcode
-        self.write(sector)        # Send the sector to write
-        self.write32(address)    # Send the 32 bits destination address (4 bytes)
-        self.write(count)        # Send how many bytes we want to write (1 byte)
-        for i in range(len(data)):    # Write data
-            self.write(data[i])
-        return self.read()[0]
+        response = -1
+        while len(data) != 0:
+            count = min(256, len(data))
+            self.write(0xfb)        # Send the write flash opcode
+            self.write(sector)      # Send the sector to write
+            self.write32(address)   # Send the 32 bits destination address (4 bytes)
+            self.write(count)       # Send how many bytes we want to write (1 byte)
+            for i in range(count):      # Write data
+                self.write(data[i])
+            address = address + count
+            data = data[count:]
+            response = self.read()
+        return response
 
     def erase_flash(self, sector):
         """Erases flash address at sector
@@ -163,7 +162,7 @@ class mbedLogo():
         """
         self.write(0xfa)        # Send the erase flash opcode
         self.write(sector)        # Send the sector to write
-        return self.read()[0]
+        return self.read()
 
     def run_command(self, command):
         """Writes compiled Logo command to the FLASH memory and runs it
@@ -174,51 +173,5 @@ class mbedLogo():
 
         """
         self.erase_flash(eSector)    # Erase flash sector
-        self.write_flash(eSector, e, len(command), command)    # Write command to flash
+        self.write_flash(eSector, e, command)    # Write command to flash
         self.write(0xfc)        # Send the run opcode
-
-#data = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
-#
-#mbed = mbedLogo()
-#
-#print "Testing communication...",
-#print mbed.test_communication()
-#
-#### Working with Memory
-#print "Reading %d bytes from 0x%0X (Memory)" % (len(data), g)
-#print mbed.read_memory(g, len(data))
-#print ' '
-#
-#print "Writing %d bytes in 0x%0X (Memory)" % (len(data), g)
-#print mbed.write_memory(g, len(data), data )
-#print ' '
-#
-#print "Reading %d bytes from 0x%0X (Memory)" % (len(data), g)
-#print mbed.read_memory(g, len(data))
-#print ' '
-#
-#print ' '
-### Working with Flash
-#print "Reading %d bytes from 0x%0X (FLASH)" % (len(data), e)
-#print mbed.read_memory(e, len(data))
-#print ' '
-#
-#print "Erasing flash"
-#print mbed.erase_flash(eSector)
-#print ' '
-#
-#print "Writing %d bytes in 0x%0X (FLASH)" % (len(data), e)
-#print mbed.write_flash(eSector, e, len(data), data)
-#print ' '
-#
-#print "Reading %d bytes from 0x%0X (FLASH)" % (len(data), e)
-#print mbed.read_memory(e, len(data))
-#print ' '
-#
-#command1 = [1, 45, 10, 9, 0]
-#mbed.run_command(command1)
-#print mbed.read()
-#
-#command2 = [1, 4, 3, 5, 0, 1, 12, 10, 9, 4, 17, 0]
-#mbed.run_command(command2)
-#print mbed.read()
